bb1e9b1d76d2bcc29d8aa2f1477c200d2693b2ab
1136314 <kilmmk@hyundai-autoever.com>
2023-04-03 13:29:23 +0900

    front  backend 연동

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+
+const path = require("path");
+
+module.exports = {
+
+
+  outputDir : path.resolve('backend/public'),
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000/api',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
+}
